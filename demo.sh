e6d29c0eaa45221bef6c8874f6b680645a68b737
Yoan Haouzi <yoan.haouzi@evaneos.com>
2019-06-07 18:01:55 +0200

    Amélioration de la clé de cryptage 5

--- demo.sh
+++ demo.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 var=$(./mine.sh)
+echo $var
 if [ "$var" == 1 ]; then
     echo "Bitcoin miné !! "
 fi
--- mine.sh
+++ mine.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-miningKey= echo 1 | md5 | tr 'A-Za-z' 'N-ZA-Mn-za-m' | md5 | md5 | md5 | openssl sha1
-echo $miningKey
+cleCrypte=$(echo "Ma clé de minage" | md5 | tr 'A-Za-z' 'N-ZA-Mn-za-m' | md5 | md5 | md5 | openssl sha1)
+echo ${cleCrypte}
