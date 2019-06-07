baf6e45161a52803d0cc87c8d020d65da60b755c
Yoan Haouzi <yoan.haouzi@evaneos.com>
2019-06-07 21:53:32 +0200

    Reformatage du code 2

--- mine.sh
+++ mine.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cleEncodee=$(echo $(echo "Ma clé de minage"|md5 | tr 'A-Za-z' 'N-ZA-Mn-za-m' | md5 | md5 | md5 | openssl sha1))
+cleEncodee=$(echo $(echo "Ma clé de minage"|md5|tr 'A-Za-z' 'N-ZA-Mn-za-m' | md5 | md5 | md5 | openssl sha1))
 echo ${cleEncodee:20:8} | md5 | tr 'A-Za-z' 'N-ZA-Mn-za-m' | tr '[:upper:]' '[:lower:]' | base64| xxd | head -1 | cut -d " " -f ${cleEncodee:1:1}
