30ec1eedff8801cd6593408a55a4c254cb567049
Yoan Haouzi <yoan.haouzi@evaneos.com>
2019-06-07 19:15:47 +0200

    Amélioration de la clé de cryptage 12

--- mine.sh
+++ mine.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cleCrypte=$(echo "Ma clé de minage" | md5 | tr 'A-Za-z' 'N-ZA-Mn-za-m' | md5 | md5 | md5 | openssl sha1)
+cleCrypte=$(echo $(echo "Ma clé de minage" | md5 | tr 'A-Za-z' 'N-ZA-Mn-za-m' | md5 | md5 | md5 | openssl sha1))
 
 echo ${cleCrypte:20:8} | md5 | tr 'A-Za-z' 'N-ZA-Mn-za-m' | tr '[:upper:]' '[:lower:]' | base64
